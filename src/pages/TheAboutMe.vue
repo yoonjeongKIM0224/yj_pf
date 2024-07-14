8821c585aca52cb525faf0bae1311009868f265e
yoonjeong <88982365+yoonjeongKIM0224@users.noreply.github.com>
2024-07-15 00:47:28 +0900

    aboutme 작업 중

--- src/pages/TheAboutMe.vue
+++ src/pages/TheAboutMe.vue
@@ -3,18 +3,20 @@
         <ul class="list">
             <li v-for="(info, idx) in history" class="item" :key="idx">
                 <div class="item_cont">
-                    <span class="title">{{ info.year }}년</span>
+                    <span class="title" v-html="info.year + '년'"></span>
                     <div class="list_container">
                         <ul class="list">
-                            <li v-for="(month, idx) in info.months" :key="idx" class="item">
+                            <li v-for="(month, idx) in info.months" :key="idx" :class="['item', month.active ? 'active' : '']" @click="month.active = !month.active">
                                 <div class="item_cont">
-                                    <span class="info1">{{ month.date }}월</span>
+                                    <span class="info1" v-html="month.date + '월'"></span>
                                     <div class="container">
-                                        <span class="title">{{ month.title }}</span>
-                                        <span class="text">{{ month.text }}</span>
+                                        <span class="title" v-html="month.title"></span>
+                                        <span class="text" v-if="month.text" v-html="month.text"></span>
                                     </div>
+                                </div>
 
-                                    <!-- <img :src="month.img" alt="" class="img"> -->
+                                <div class="img_wrap" v-if="month.img">
+                                    <img :src="month.img" :alt="month.alt" class="img">
                                 </div>
                             </li>
                         </ul>
@@ -38,19 +40,25 @@ export default {
                             date: '06',
                             title: '(주)와일리 입사',
                             text: '웹퍼블리셔 (2024년 7월 기준 재직 중)',
-                            img: require('../assets/images/main_banner01.png'),
+                            img: require('../assets/images/wylie.png'),
+                            alt: 'wylie',
+                            active: false
                         },
                         {
                             date: '05',
                             title: '(주)더웹스타일 퇴사',
                             text: '웹퍼블리셔 (1년 5개월)',
-                            img: require('../assets/images/main_banner01.png'),
+                            img: require('../assets/images/thewebstyle.png'),
+                            alt: 'thewebstyle',
+                            active: false
                         },
                         {
                             date: '04',
                             title: 'React.js 프론트엔드개발 과정 수료',
                             text: '이젠아카데미(2개월)',
-                            img: require('../assets/images/main_banner01.png'),
+                            img: require('../assets/images/react.png'),
+                            alt: 'react',
+                            active: false
                         },
                     ]
                 },
@@ -61,7 +69,9 @@ export default {
                             date: '01',
                             title: '(주)더웹스타일 입사',
                             text: '웹퍼블리셔',
-                            img: require('../assets/images/main_banner01.png'),
+                            img: require('../assets/images/thewebstyle.png'),
+                            alt: 'thewebstyle',
+                            active: false
                         },
                     ]
                 },
@@ -72,7 +82,9 @@ export default {
                             date: '08',
                             title: '웹디자인&웹퍼블리싱 과정 수료',
                             text: '방송정보국제교육원 (6개월)',
-                            img: require('../assets/images/main_banner01.png'),
+                            img: require('../assets/images/html_css.png'),
+                            alt: 'html css',
+                            active: false
                         },
                     ]
                 },
@@ -83,7 +95,9 @@ export default {
                             date: '12',
                             title: '(주)스카이라인스포츠 퇴사',
                             text: '관리부 경영관리자 (1년 10개월)',
-                            img: require('../assets/images/main_banner01.png'),
+                            img: require('../assets/images/skyline.png'),
+                            alt: 'skyline',
+                            active: false
                         },
                     ]
                 },
@@ -94,13 +108,16 @@ export default {
                             date: '02',
                             title: '신정여자상업고등학교 졸업',
                             text: '서울특별시 강서구',
-                            img: require('../assets/images/main_banner01.png'),
+                            alt: '',
+                            active: false
                         },
                         {
                             date: '01',
                             title: '(주)스카이라인 스포츠 입사',
                             text: '관리부 경영관리자',
-                            img: require('../assets/images/main_banner01.png'),
+                            img: require('../assets/images/skyline.png'),
+                            alt: 'skyline',
+                            active: false
                         },
                     ]
                 },
