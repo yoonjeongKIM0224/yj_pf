27dfa73766a59455a4b94dd9609e459083f8b78f
김윤정 <yjkim2@wylie.co.kr>
2024-08-13 18:31:58 +0900

    컴포넌트 정리

--- src/pages/TheComponents.vue
+++ src/pages/TheComponents.vue
@@ -1,22 +1,43 @@
 <template>
     <TheTab type="2" :tabs="tabs" :handler="true" @tabSelected="tabSelected = $event">
         <template v-slot:tabPanel1>
-            <iframe height="600" style="width: 100%;" scrolling="no" title="cat" src="https://codepen.io/yoonjeongkim0224/embed/vYJqxVx?default-tab=html%2Cresult" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
-            See the Pen <a href="https://codepen.io/yoonjeongkim0224/pen/vYJqxVx">
-            cat</a> by yoonjeong (<a href="https://codepen.io/yoonjeongkim0224">@yoonjeongkim0224</a>)
+            <iframe height="600" style="width: 100%;" scrolling="no" title="Untitled" src="https://codepen.io/yoonjeongkim0224/embed/poXewEQ?default-tab=html%2Cresult" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
+            See the Pen <a href="https://codepen.io/yoonjeongkim0224/pen/poXewEQ">
+            Untitled</a> by yoonjeong (<a href="https://codepen.io/yoonjeongkim0224">@yoonjeongkim0224</a>)
             on <a href="https://codepen.io">CodePen</a>.
             </iframe>
         </template>
         <template v-slot:tabPanel2>
-            <iframe height="600" style="width: 100%;" scrolling="no" title="skull" src="https://codepen.io/yoonjeongkim0224/embed/VwMedvx?default-tab=html%2Cresult" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
-            See the Pen <a href="https://codepen.io/yoonjeongkim0224/pen/VwMedvx">
-            skull</a> by yoonjeong (<a href="https://codepen.io/yoonjeongkim0224">@yoonjeongkim0224</a>)
+            <iframe height="600" style="width: 100%;" scrolling="no" title="Untitled" src="https://codepen.io/yoonjeongkim0224/embed/MWMpJPg?default-tab=html%2Cresult" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
+            See the Pen <a href="https://codepen.io/yoonjeongkim0224/pen/MWMpJPg">
+            Untitled</a> by yoonjeong (<a href="https://codepen.io/yoonjeongkim0224">@yoonjeongkim0224</a>)
             on <a href="https://codepen.io">CodePen</a>.
+            </iframe>
+        </template>
+        <template v-slot:tabPanel3>
+            <iframe height="600" style="width: 100%;" scrolling="no" title="dark mode" src="https://codepen.io/yoonjeongkim0224/embed/VwOomRM?default-tab=html%2Cresult" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
+                See the Pen <a href="https://codepen.io/yoonjeongkim0224/pen/VwOomRM">
+                dark mode</a> by yoonjeong (<a href="https://codepen.io/yoonjeongkim0224">@yoonjeongkim0224</a>)
+                on <a href="https://codepen.io">CodePen</a>.
+            </iframe>
+        </template>
+        <template v-slot:tabPanel4>
+            <iframe height="600" style="width: 100%;" scrolling="no" title="js" src="https://codepen.io/yoonjeongkim0224/embed/BaPJwrp?default-tab=html%2Cresult" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
+                See the Pen <a href="https://codepen.io/yoonjeongkim0224/pen/BaPJwrp">
+                js</a> by yoonjeong (<a href="https://codepen.io/yoonjeongkim0224">@yoonjeongkim0224</a>)
+                on <a href="https://codepen.io">CodePen</a>.
+            </iframe>
+        </template>
+        <template v-slot:tabPanel5>
+            <iframe height="600" style="width: 100%;" scrolling="no" title="find btn" src="https://codepen.io/yoonjeongkim0224/embed/OJOJrmm?default-tab=html%2Cresult" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
+                See the Pen <a href="https://codepen.io/yoonjeongkim0224/pen/OJOJrmm">
+                find btn</a> by yoonjeong (<a href="https://codepen.io/yoonjeongkim0224">@yoonjeongkim0224</a>)
+                on <a href="https://codepen.io">CodePen</a>.
             </iframe>
         </template>
     </TheTab>
 </template>
-  
+
 <script>
 import TheTab from '@/components/TheTab.vue';
 
@@ -28,6 +49,21 @@ export default {
             tabs: [
                 {
                     text: 'checkbox',
+                    itemClass: '',
+                    disabled: false,
+                },
+                {
+                    text: 'radio',
+                    itemClass: '',
+                    disabled: false,
+                },
+                {
+                    text: 'radio',
+                    itemClass: '',
+                    disabled: false,
+                },
+                {
+                    text: 'radio',
                     itemClass: '',
                     disabled: false,
                 },
