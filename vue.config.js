b00110fb076accfb7d8855acbbd3b517b2d55c75
YJ <gnoejnooy@gmail.com>
2025-05-15 13:57:27 +0900

    업뎃뎃

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/yoonjeongKIM0224.github.io'
 })
